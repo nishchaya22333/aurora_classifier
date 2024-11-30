0136db499cf9d14059e58e3ed0d897e81bfd2e23
nishchaya22333 <132191677+nishchaya22333@users.noreply.github.com>
2024-11-30 17:00:34 +0530

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-#-------------------------------------------------------------------------------
-#Unless stated otherwise, all data in the OATH Dataset is licensed under a
-#<a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons 4.0 
-#Attribution License (CC BY 4.0)</a> and the accompanying source code is 
-#licensed under a <a href="https://opensource.org/licenses/BSD-2-Clause">
-#BSD-2-Clause License</a>.
-#In particular, all actual image data included in the tarball are modified from 
-#the <a href="https://www.nasa.gov/mission_pages/themis/spacecraft/asi.html">
-#THEMIS all-sky imagers </a>.
-#
-#We thank H. Frey for giving us permission to include these data.
-#Copyright for these data remains with NASA.
-#
-#We acknowledge NASA contract NAS5-02099 and V. Angelopoulos for use of data 
-#from the THEMIS Mission. Specifically: S. Mende and E. Donovan for use of the
-#ASI data, the CSA for logistical support in fielding and data retrieval from
-#the GBO stations, and NSF for support of GIMNAST through grant AGS-1004736.
-#-------------------------------------------------------------------------------
-
 mkdir ../images/cropped_scaled_rotated
 linecount=0
 headerlength=19
